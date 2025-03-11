a77ea15bf17e4dfac72e8ad9568745f4993fa260
sgoel <shobhit.goel@iqvia.com>
2025-03-12 00:25:42 +0530

    changes added after dvc setup

--- src/model_building.py
+++ src/model_building.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split 
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
 
 
 data=pd.read_csv('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/data/cleaned_rawfile.csv')
@@ -21,7 +20,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3,random_state=42)
 
 X_train=scale.fit_transform(X_train)
 #X_test=scale.transform(X_test)
-new_model=RandomForestRegressor(max_depth=10, max_features='log2')
+new_model=RandomForestClassifier(max_depth=10, max_features='sqrt')
 new_model.fit(X_train,y_train)
 
 import pickle
---
+++ src/model_evaluation.py
@@ -0,0 +1,35 @@
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+import json
+import os
+
+with open('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/model/telecom_user.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+y_test=pd.read_csv('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/data/y_test.csv')
+x_test=pd.read_csv('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/data/x_test.csv')
+scale=StandardScaler()
+x_test=scale.fit_transform(x_test)
+y_pred=loaded_model.predict(x_test)
+
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1_score = f1_score(y_test, y_pred)
+
+metrics_dict = {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'f1_score': f1_score
+        }
+if not os.path.exists('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/metrics'):
+    os.makedirs('C:/Users/sgoel/IVP/New folder___/ML_model_training_using_MLOPS_dvc_and_dvclive/metrics')
+else:
+  print("folder already there metrcics one")
+
+with open ('metrics/metric.json','w') as json_file:
+    json.dump(metrics_dict,json_file,indent=4)
